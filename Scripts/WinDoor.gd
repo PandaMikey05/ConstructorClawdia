003aef3a62a5647702a86b3d9315028fb8806604
Michael <michael.hajasz.05@gmail.com>
2024-04-20 19:51:50 -0400

    fixed win door script

--- Scripts/WinDoor.gd
+++ Scripts/WinDoor.gd
@@ -7,4 +7,4 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _input(event):
 	if event.is_action_pressed("Throw"):
-		get_tree().change_scene_to_file("res://WinScreenNextLevel.tscn")
+		get_tree().change_scene_to_file("res://Scene/WinScreenNextLevel.tscn")
